5912ef65cbb8b32c60c7481f48cbb97a75d35737
SEFR <sebastien.francois@octo.com>
2023-10-31 15:39:10 +0100

    test: replaced mocha chai sinon with vitest
    style: installed prettier to format code
    
    refactor: removed use of deprecated method of estree
    
    Release-As: 1.3.13

--- src/no-onion-architecture-violation.ts
+++ src/no-onion-architecture-violation.ts
@@ -1,46 +1,66 @@
-import { ImportDeclaration } from "estree";
-import { Rule } from "eslint";
+import { ImportDeclaration } from 'estree';
+import { Rule } from 'eslint';
 
 export class NoOnionArchitectureViolation {
-	private static MESSAGE = "This import violates Onion Architecture principles. Read more about this architecture : " +
-		"https://medium.com/expedia-group-tech/onion-architecture-deed8a554423";
+  private static MESSAGE =
+    'This import violates Onion Architecture principles. Read more about this architecture : ' +
+    'https://medium.com/expedia-group-tech/onion-architecture-deed8a554423';
 
-	private static INFRASTRUCTURE_LAYER = new RegExp(/infras?|infrastructures?/);
-	private static APPLICATION_SERVICE_LAYER = new RegExp(/(applications?-services?)|(applications?)/);
-	private static DOMAIN_SERVICE_LAYER = new RegExp(/domains?\/services?|domains?-services?/);
-	private static DOMAIN_MODEL_LAYER = new RegExp(/domains?\/models?|domains?-models?/);
+  private static INFRASTRUCTURE_LAYER = new RegExp(/infras?|infrastructures?/);
+  private static APPLICATION_SERVICE_LAYER = new RegExp(
+    /(applications?-services?)|(applications?)/
+  );
+  private static DOMAIN_SERVICE_LAYER = new RegExp(/domains?\/services?|domains?-services?/);
+  private static DOMAIN_MODEL_LAYER = new RegExp(/domains?\/models?|domains?-models?/);
 
-	public static create(context: Rule.RuleContext): Rule.RuleListener {
-		return {
-			ImportDeclaration: (node: ImportDeclaration & Rule.NodeParentExtension): void => {
-				NoOnionArchitectureViolation.checkNoCrossDomainImport(context, node);
-			}
-		};
-	}
+  public static create(context: Rule.RuleContext): Rule.RuleListener {
+    return {
+      ImportDeclaration: (node: ImportDeclaration & Rule.NodeParentExtension): void => {
+        NoOnionArchitectureViolation.checkNoCrossDomainImport(context, node);
+      }
+    };
+  }
 
-	public static checkNoCrossDomainImport(context: Rule.RuleContext, node: ImportDeclaration & Rule.NodeParentExtension): void {
-		const currentFilePath = context.getPhysicalFilename();
-		const currentImportPath = <string>node.source.value;
+  public static checkNoCrossDomainImport(
+    context: Rule.RuleContext,
+    node: ImportDeclaration & Rule.NodeParentExtension
+  ): void {
+    const currentFilePath = context.physicalFilename;
+    const currentImportPath = <string>node.source.value;
 
-		if (this.isImportFromLayer(currentImportPath, this.INFRASTRUCTURE_LAYER) && this.isFileFromLayers(currentFilePath, this.APPLICATION_SERVICE_LAYER, this.DOMAIN_SERVICE_LAYER, this.DOMAIN_MODEL_LAYER)) {
-			context.report({ node, message: this.MESSAGE });
-		} else if (this.isImportFromLayer(currentImportPath, this.APPLICATION_SERVICE_LAYER) && this.isFileFromLayers(currentFilePath, this.DOMAIN_SERVICE_LAYER, this.DOMAIN_MODEL_LAYER)) {
-			context.report({ node, message: this.MESSAGE });
-		} else if (this.isImportFromLayer(currentImportPath, this.DOMAIN_SERVICE_LAYER) && this.isFileFromLayers(currentFilePath, this.DOMAIN_MODEL_LAYER)) {
-			context.report({ node, message: this.MESSAGE });
-		}
-	}
+    if (
+      this.isImportFromLayer(currentImportPath, this.INFRASTRUCTURE_LAYER) &&
+      this.isFileFromLayers(
+        currentFilePath,
+        this.APPLICATION_SERVICE_LAYER,
+        this.DOMAIN_SERVICE_LAYER,
+        this.DOMAIN_MODEL_LAYER
+      )
+    ) {
+      context.report({ node, message: this.MESSAGE });
+    } else if (
+      this.isImportFromLayer(currentImportPath, this.APPLICATION_SERVICE_LAYER) &&
+      this.isFileFromLayers(currentFilePath, this.DOMAIN_SERVICE_LAYER, this.DOMAIN_MODEL_LAYER)
+    ) {
+      context.report({ node, message: this.MESSAGE });
+    } else if (
+      this.isImportFromLayer(currentImportPath, this.DOMAIN_SERVICE_LAYER) &&
+      this.isFileFromLayers(currentFilePath, this.DOMAIN_MODEL_LAYER)
+    ) {
+      context.report({ node, message: this.MESSAGE });
+    }
+  }
 
-	private static isImportFromLayer(currentImportPath: string, layer: RegExp): boolean {
-		return currentImportPath.match(layer) !== null;
-	}
+  private static isImportFromLayer(currentImportPath: string, layer: RegExp): boolean {
+    return currentImportPath.match(layer) !== null;
+  }
 
-	private static isFileFromLayers(currentFilePath: string, ...layers: Array<RegExp>): boolean {
-		for (const layer of layers) {
-			if (currentFilePath.match(layer)) {
-				return true;
-			}
-		}
-		return false;
-	}
+  private static isFileFromLayers(currentFilePath: string, ...layers: Array<RegExp>): boolean {
+    for (const layer of layers) {
+      if (currentFilePath.match(layer)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
--- test/no-hexagonal-architecture-violation.test.ts
+++ test/no-hexagonal-architecture-violation.test.ts
@@ -1,196 +1,198 @@
-import { expect, sinon, StubbedType, stubInterface } from "@sefr/test";
-import { NoHexagonalArchitectureViolation } from "../src/no-hexagonal-architecture-violation";
-import { Rule } from "eslint";
-import { ImportDeclaration } from "estree";
+import { beforeEach, describe, expect, it } from 'vitest';
+import { NoHexagonalArchitectureViolation } from '../src/no-hexagonal-architecture-violation';
+import { Rule } from 'eslint';
+import { ImportDeclaration } from 'estree';
+import { mock } from 'vitest-mock-extended';
 
-const message = "This import violates Hexagonal Architecture principles. " +
-	"Read more about this architecture : https://medium.com/idealo-tech-blog/hexagonal-ports-adapters-architecture-e3617bcf00a0";
-let ruleContext: StubbedType<Rule.RuleContext>;
-let node: StubbedType<ImportDeclaration & Rule.NodeParentExtension>;
+const message =
+  'This import violates Hexagonal Architecture principles. ' +
+  'Read more about this architecture : https://medium.com/idealo-tech-blog/hexagonal-ports-adapters-architecture-e3617bcf00a0';
+let ruleContext: Rule.RuleContext;
+let node: ImportDeclaration & Rule.NodeParentExtension;
 
-describe("NoHexagonalArchitectureViolation", () => {
-	beforeEach(() => {
-		ruleContext = stubInterface<Rule.RuleContext>(sinon);
-		node = stubInterface<ImportDeclaration & Rule.NodeParentExtension>(sinon);
-	});
+describe('NoHexagonalArchitectureViolation', () => {
+  beforeEach(() => {
+    ruleContext = mock<Rule.RuleContext>();
+    node = mock<ImportDeclaration & Rule.NodeParentExtension>();
+  });
 
-	context("When current file belongs to the hexagon", () => {
-		context("and we try to import a module from the user-side", () => {
-			it("reports the bug", () => {
-				// Given
-				ruleContext.getPhysicalFilename.returns("root/path/to/core/current-file.ts");
-				node.source.value = "root/path/to/user-side/some-module";
+  describe('When current file belongs to the hexagon', () => {
+    describe('and we try to import a module from the user-side', () => {
+      it('reports the bug', () => {
+        // Given
+        ruleContext.physicalFilename = 'root/path/to/core/current-file.ts';
+        node.source.value = 'root/path/to/user-side/some-module';
 
-				// When
-				NoHexagonalArchitectureViolation.check(ruleContext, node);
+        // When
+        NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-				// Then
-				expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-			});
-		});
+        // Then
+        expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+      });
+    });
 
-		context("and we try to import a module from the server-side", () => {
-			it("reports the bug", () => {
-				// Given
-				ruleContext.getPhysicalFilename.returns("root/path/to/core/current-file.ts");
-				node.source.value = "root/path/to/server-side/some-module";
+    describe('and we try to import a module from the server-side', () => {
+      it('reports the bug', () => {
+        // Given
+        ruleContext.physicalFilename = 'root/path/to/core/current-file.ts';
+        node.source.value = 'root/path/to/server-side/some-module';
 
-				// When
-				NoHexagonalArchitectureViolation.check(ruleContext, node);
+        // When
+        NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-				// Then
-				expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-			});
-		});
-	});
+        // Then
+        expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+      });
+    });
+  });
 
-	context("When current file belongs to the user-side", () => {
-		beforeEach(() => {
-			ruleContext.getPhysicalFilename.returns("root/path/to/user-side/current-file.ts");
-		});
+  describe('When current file belongs to the user-side', () => {
+    beforeEach(() => {
+      ruleContext.physicalFilename = 'root/path/to/user-side/current-file.ts';
+    });
 
-		context("and we try to import a module from the hexagon", () => {
-			context("and the hexagon is called 'hexagon' as a folder", () => {
-				it("does nothing", () => {
-					// Given
-					node.source.value = "root/path/to/hexagon/some-module";
+    describe('and we try to import a module from the hexagon', () => {
+      describe("and the hexagon is called 'hexagon' as a folder", () => {
+        it('does nothing', () => {
+          // Given
+          node.source.value = 'root/path/to/hexagon/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.not.have.been.called;
-				});
-			});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledTimes(0);
+        });
+      });
 
-			context("and the hexagon is called 'core' as a folder", () => {
-				it("does nothing", () => {
-					// Given
-					node.source.value = "root/path/to/core/some-module";
+      describe("and the hexagon is called 'core' as a folder", () => {
+        it('does nothing', () => {
+          // Given
+          node.source.value = 'root/path/to/core/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.not.have.been.called;
-				});
-			});
-		});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledTimes(0);
+        });
+      });
+    });
 
-		context("and we try to import a module from the server-side", () => {
-			context("and the server-side is called 'server-side' as a folder", () => {
-				it("reports the bug", () => {
-					// Given
-					node.source.value = "root/path/to/server-side/some-module";
+    describe('and we try to import a module from the server-side', () => {
+      describe("and the server-side is called 'server-side' as a folder", () => {
+        it('reports the bug', () => {
+          // Given
+          node.source.value = 'root/path/to/server-side/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-				});
-			});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+        });
+      });
 
-			context("and the server-side is called 'infrastructure' as a folder", () => {
-				it("reports the bug", () => {
-					// Given
-					node.source.value = "root/path/to/infrastructure/some-module";
+      describe("and the server-side is called 'infrastructure' as a folder", () => {
+        it('reports the bug', () => {
+          // Given
+          node.source.value = 'root/path/to/infrastructure/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-				});
-			});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+        });
+      });
 
-			context("and the server-side is called 'driven-side' as a folder", () => {
-				it("reports the bug", () => {
-					// Given
-					node.source.value = "root/path/to/driven-side/some-module";
+      describe("and the server-side is called 'driven-side' as a folder", () => {
+        it('reports the bug', () => {
+          // Given
+          node.source.value = 'root/path/to/driven-side/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-				});
-			});
-		});
-	});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+        });
+      });
+    });
+  });
 
-	context("When current file belongs to the server-side", () => {
-		beforeEach(() => {
-			ruleContext.getPhysicalFilename.returns("root/path/to/server-side/current-file.ts");
-		});
+  describe('When current file belongs to the server-side', () => {
+    beforeEach(() => {
+      ruleContext.physicalFilename = 'root/path/to/server-side/current-file.ts';
+    });
 
-		context("and we try to import a module from the hexagon", () => {
-			context("and the hexagon is called 'hexagon' as a folder", () => {
-				it("does nothing", () => {
-					// Given
-					node.source.value = "root/path/to/hexagon/some-module";
+    describe('and we try to import a module from the hexagon', () => {
+      describe("and the hexagon is called 'hexagon' as a folder", () => {
+        it('does nothing', () => {
+          // Given
+          node.source.value = 'root/path/to/hexagon/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.not.have.been.called;
-				});
-			});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledTimes(0);
+        });
+      });
 
-			context("and the hexagon is called 'core' as a folder", () => {
-				it("does nothing", () => {
-					// Given
-					node.source.value = "root/path/to/core/some-module";
+      describe("and the hexagon is called 'core' as a folder", () => {
+        it('does nothing', () => {
+          // Given
+          node.source.value = 'root/path/to/core/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.not.have.been.called;
-				});
-			});
-		});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledTimes(0);
+        });
+      });
+    });
 
-		context("and we try to import a module from the user-side", () => {
-			context("and the user-side is called 'user-side' as a folder", () => {
-				it("reports the bug", () => {
-					// Given
-					node.source.value = "root/path/to/user-side/some-module";
+    describe('and we try to import a module from the user-side', () => {
+      describe("and the user-side is called 'user-side' as a folder", () => {
+        it('reports the bug', () => {
+          // Given
+          node.source.value = 'root/path/to/user-side/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-				});
-			});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+        });
+      });
 
-			context("and the user-side is called 'application' as a folder", () => {
-				it("reports the bug", () => {
-					// Given
-					node.source.value = "root/path/to/application/some-module";
+      describe("and the user-side is called 'application' as a folder", () => {
+        it('reports the bug', () => {
+          // Given
+          node.source.value = 'root/path/to/application/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-				});
-			});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+        });
+      });
 
-			context("and the user-side is called 'driver-side' as a folder", () => {
-				it("reports the bug", () => {
-					// Given
-					node.source.value = "root/path/to/driver-side/some-module";
+      describe("and the user-side is called 'driver-side' as a folder", () => {
+        it('reports the bug', () => {
+          // Given
+          node.source.value = 'root/path/to/driver-side/some-module';
 
-					// When
-					NoHexagonalArchitectureViolation.check(ruleContext, node);
+          // When
+          NoHexagonalArchitectureViolation.check(ruleContext, node);
 
-					// Then
-					expect(ruleContext.report).to.have.been.calledOnceWith({ message, node });
-				});
-			});
-		});
-	});
+          // Then
+          expect(ruleContext.report).toHaveBeenCalledWith({ message, node });
+        });
+      });
+    });
+  });
 });
